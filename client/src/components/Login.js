5a541cc9d5fc9530529b97ddbbfcda61bdb48f69
Mitchell Wright <mitchellbwright@gmail.com>
2020-09-11 17:59:55 -0600

    set up initial data fetch and set colorList

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -16,7 +16,6 @@ const Login = () => {
     axios
       .post("http://localhost:5000/api/login", formInputs)
       .then((res) => {
-        console.log(res);
         localStorage.setItem("token", res.data.payload);
         history.push("/bubble-page");
       })
