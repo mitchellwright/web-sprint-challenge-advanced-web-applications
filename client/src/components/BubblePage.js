f2b0a05f0d70bd5806e87211db4d74c07121b06f
Mitchell Wright <mitchellbwright@gmail.com>
2020-09-11 19:45:46 -0600

    finish up tests

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+// import { axiosWithAuth } from "../utils/axiosWithAuth";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
