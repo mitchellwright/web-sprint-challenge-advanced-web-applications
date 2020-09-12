7d24a2f07c76c90817da069e872ef9ac3f4b9579
Mitchell Wright <mitchellbwright@gmail.com>
2020-09-11 18:25:51 -0600

    add edit functionality

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -7,7 +7,6 @@ const initialColor = {
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -21,6 +20,13 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then((res) => {
+        const newColors = colors.filter((color) => color.id !== colorToEdit.id);
+        updateColors([...newColors, { ...colorToEdit }]);
+      })
+      .catch((err) => console.log(err));
   };
 
   const deleteColor = (color) => {
@@ -28,7 +34,6 @@ const ColorList = ({ colors, updateColors }) => {
     axiosWithAuth()
       .delete(`http://localhost:5000/api/colors/${color.id}`)
       .then((res) => {
-        console.log(res);
         updateColors(colors.filter((thisColor) => thisColor.id !== color.id));
       })
       .catch((err) => console.error(err));
